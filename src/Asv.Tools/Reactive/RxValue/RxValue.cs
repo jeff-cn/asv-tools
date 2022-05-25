85db588391c9c14a18007887bb4bf47d6384968c
Alexey <248997@gmail.com>
2022-05-25 16:37:41 +0500

    Submodule update

--- src/Asv.Tools/Reactive/RxValue/RxValue.cs
+++ src/Asv.Tools/Reactive/RxValue/RxValue.cs
@@ -6,6 +6,16 @@ namespace Asv.Tools
 {
     public class RxValue<TValue> : DisposableOnce, IRxEditableValue<TValue>
     {
+        public RxValue()
+        {
+            
+        }
+
+        public RxValue(TValue initValue) : this()
+        {
+            _value = initValue;
+        }
+
         private readonly Subject<TValue> _subject = new();
         private TValue _value;
 
