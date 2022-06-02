c648ac45fede5f00791ada96f0f036b86cc43d91
Alexey <248997@gmail.com>
2022-06-02 18:13:37 +0500

    [Avalonia] Add new view model with validation and dispose

---
+++ src/Asv.Tools.Avalonia/ViewModels/AsyncActionViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using ReactiveUI;
+
+namespace Asv.Tools.Avalonia
+{
+    
+}
