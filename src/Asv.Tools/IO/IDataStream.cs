96e6a11d51b8efed6d977c6f847ac8e955d89355
Alexey <248997@gmail.com>
2022-08-19 14:06:46 +0500

    Data stream helper add ext

--- src/Asv.Tools/IO/IDataStream.cs
+++ src/Asv.Tools/IO/IDataStream.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Buffers;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,14 +16,21 @@ namespace Asv.Tools
 
     public static class DataStreamHelper
     {
-        public static Task<bool> Send(this IDataStream src, ISizedSpanSerializable data,CancellationToken cancel = default)
+        public static Task<bool> Send(this IDataStream src, ISizedSpanSerializable data,  CancellationToken cancel = default)
+        {
+            return Send(src, data, out var sended, cancel);
+        }
+
+        public static Task<bool> Send(this IDataStream src, ISizedSpanSerializable data,out int byteSended, CancellationToken cancel = default)
         {
             var size = data.GetByteSize();
             var array = ArrayPool<byte>.Shared.Rent(size);
             var span = new Span<byte>(array,0,size);
             try
             {
+                byteSended = span.Length;
                 data.Serialize(ref span);
+                byteSended -= span.Length;
                 return src.Send(array, size, cancel);
             }
             finally
